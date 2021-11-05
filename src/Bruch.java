4b023b42f6cea4b7bad2489b9bc9968c2bed5b14
connykrautstingl <s44784@edu.campus02.at>
2021-11-05 17:49:43 +0100

    Implementierung print

--- src/Bruch.java
+++ src/Bruch.java
@@ -26,7 +26,7 @@ public class Bruch {
 
     public String print() {
         // TODO: Die Methode soll den Bruch als Text retournieren z.B.: "5 / 7"
-        return "";
+        return denominator + " / " + numerator;
     }
 
     public Bruch multiplicate(Bruch b2) {
